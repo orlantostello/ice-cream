638dd1b98e8f86a66a8db52d56a5de5f755cf0d9
NikJulia <julia.tryrog@gmail.com>
2021-04-27 20:11:34 +0300

    Обновляет скрипт для модального окна

--- src/js/modal.js
+++ src/js/modal.js
@@ -2,13 +2,15 @@
   const refs = {
       openModalBtn: document.querySelector('[data-modal-open]'),
       openModalMobBtn: document.querySelector('[mobile-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
+      closeModalBtn: document.querySelector('[data-modal-close]'),
+    submitModalBtn: document.querySelector('[submit-modal-close]'),
     modal: document.querySelector('[data-modal]'),
   };
 
     refs.openModalBtn.addEventListener('click', toggleModal);
     refs.openModalMobBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+    refs.submitModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
     document.body.classList.toggle('modal-open');
